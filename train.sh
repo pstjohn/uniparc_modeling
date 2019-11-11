b2390c7144e43dd0cfbe20061845812bc879821e
Peter St. John <peterc.stjohn@gmail.com>
2019-11-11 14:25:45 -0700

    adding 10M model

--- train.sh
+++ train.sh
@@ -7,4 +7,4 @@
 
 export LD_LIBRARY_PATH=$HOME/lib:$LD_LIBRARY_PATH
 cd /scratch/pstjohn/uniparc/
-srun spm_train --pad_id=0 --unk_id=1 --bos_id=2 --eos_id=3 --user_defined_symbols="[MASK]" --input=sequences_train.txt --model_prefix=uniparc_5M --vocab_size=32000 --character_coverage=1.0 --model_type=unigram --input_sentence_size=5000000
+srun spm_train --pad_id=0 --unk_id=1 --bos_id=2 --eos_id=3 --user_defined_symbols="[MASK]" --input=sequences_train_no_BZUX.txt --model_prefix=uniparc_10M_no_BZUX --vocab_size=32000 --character_coverage=1.0 --model_type=unigram --input_sentence_size=5000000
