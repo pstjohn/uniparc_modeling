3d95b701c210af7c980f24f46f1311692e4ddbc1
Peter St. John <peterc.stjohn@gmail.com>
2019-11-11 14:23:24 -0700

    refactoring as library

---
+++ bert/dataset.py
@@ -0,0 +1,96 @@
+import numpy as np
+import tensorflow as tf
+import sentencepiece as spm
+
+
+def create_masked_input_dataset(language_model_path,
+                                sequence_path,
+                                max_sequence_length=512,
+                                batch_size=20,
+                                buffer_size=1024,
+                                vocab_size=32000,
+                                mask_index=4,
+                                vocab_start=5,
+                                fix_sequence_length=False):
+
+
+    sp = spm.SentencePieceProcessor()
+    sp.Load(language_model_path)
+
+    def sp_encode(line_tensor):
+        encoded_array = np.asarray(
+            sp.SampleEncodeAsIds(line_tensor.numpy(), nbest_size=-1, alpha=0.5))
+
+        # If the protein sequence is too long, take a random slice.
+        if len(encoded_array) > max_sequence_length:
+            offset = np.random.randint(
+                low=0, high=len(encoded_array) - max_sequence_length + 1)
+            encoded_array = encoded_array[offset:(offset + max_sequence_length)]
+
+        return encoded_array
+
+    def sp_decode(line_tensor):
+        return sp.DecodeIds(line_tensor.numpy().tolist())
+
+    def sp_encode_tf(line_tensor):
+        return tf.py_function(sp_encode, inp=[line_tensor], Tout=[tf.int32,])
+
+    def mask_input(input_tensor):
+        """ Randomly mask the input tensor according to the formula perscribed by BERT. 
+        Randomly masks 15% of input tokens, with 80% recieving the [MASK] token,
+        10% randomized, 10% left unchanged. 
+        
+        Returns
+        -------
+        
+        masked_tensor: (batch_size, seq_length) 
+            Tensor with masked values
+        input_tensor: (batch_size, seq_length)
+            Original input tensor (true values)
+        input_mask: (batch_size, seq_length)
+            Boolean mask that selects the desired inputs.    
+        """
+
+        mask_score = tf.random.uniform(input_tensor.shape, maxval=1, dtype=tf.float32)
+        input_mask = mask_score < .15
+
+        # Mask with [MASK] token 80% of the time
+        mask_mask = mask_score <= 0.15 * 0.8 
+
+        # Mask with random token 10% of the time
+        mask_random = (mask_score >= 0.15 * 0.9) & input_mask
+
+        # Tensors to replace with where input is masked or randomized
+        mask_value_tensor = tf.ones(input_tensor.shape, dtype=tf.int32) * mask_index
+        random_value_tensor = tf.random.uniform(
+            input_tensor.shape, minval=vocab_start, maxval=vocab_size, dtype=tf.int32)
+        pad_value_tensor = tf.zeros(input_tensor.shape, dtype=tf.int32)
+
+        # Use the replacements to mask the input tensor
+        masked_tensor = tf.where(mask_mask, mask_value_tensor, input_tensor)
+        masked_tensor = tf.where(mask_random, random_value_tensor, masked_tensor)
+        
+        # Set true values to zero (pad value) where not masked
+        true_tensor = tf.where(input_mask, input_tensor, pad_value_tensor)
+
+        return masked_tensor, true_tensor
+
+    def mask_input_tf(input_tensor):
+        return tf.py_function(mask_input, inp=[input_tensor], Tout=[tf.int32, tf.int32])
+
+
+    valid_data = tf.data.TextLineDataset(sequence_path)
+
+    encoded_data = valid_data\
+        .map(sp_encode_tf, num_parallel_calls=tf.data.experimental.AUTOTUNE)\
+        .map(mask_input_tf, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+    
+    # This argument controls whether to fix the size of the sequences
+    tf_seq_len = -1 if not fix_sequence_length else max_sequence_length
+
+    encoded_data = encoded_data\
+        .shuffle(buffer_size=buffer_size)\
+        .padded_batch(batch_size, padded_shapes=([tf_seq_len], [tf_seq_len]))
+
+    return encoded_data
---
+++ bert/layers.py
@@ -0,0 +1,220 @@
+import tensorflow as tf
+import tensorflow.keras.backend as K
+from tensorflow.keras import layers
+from tensorflow.keras.callbacks import Callback
+
+import numpy as np
+
+initializer = lambda: tf.keras.initializers.TruncatedNormal(stddev=0.02)
+
+def gelu(x):
+    """
+    Gelu activation from arXiv:1606.08415.
+    """
+    cdf = 0.5 * (
+        1.0 + tf.tanh(np.sqrt(2.0 / np.pi) * (x + 0.044715 * tf.pow(x, 3))))
+    return x * cdf
+
+
+class PositionEmbedding(layers.Embedding):
+    """ Return masked embeddings according to the position index of each input
+    """
+        
+    def call(self, inputs):
+        idx = tf.cast(tf.where(inputs != 0), dtype=tf.int32)
+        padded_position_indicies = tf.scatter_nd(
+            idx, idx[:, 1] + 1, tf.shape(inputs))
+        return super(PositionEmbedding, self).call(padded_position_indicies)
+
+
+class TokenEmbedding(layers.Embedding):
+    """ Slight modification of original keras embedding layer in order to
+    permit re-use of weights when converting final embeddings to logit
+    predictions. """
+    
+    def call(self, inputs, transpose=False):
+        if not transpose:
+            return super(TokenEmbedding, self).call(inputs)
+        else:
+            return tf.matmul(inputs, self.embeddings, transpose_b=True)
+        
+
+class Attention(layers.Layer):
+    """ Implements a single head of the multi-head attention transformer model.
+    Includes the preceeding dense layers on the value, key, and query matrices
+    """
+
+    def __init__(self, units, dropout=0.2, **kwargs):
+        super(Attention, self).__init__(**kwargs)
+        self.units = units
+        self.dropout = dropout
+        
+    def build(self, input_shape):       
+        self.query_layer = layers.Dense(
+            self.units, kernel_initializer=initializer(), name='query')
+        self.key_layer = layers.Dense(
+            self.units, kernel_initializer=initializer(), name='key')
+        self.value_layer = layers.Dense(
+            self.units, kernel_initializer=initializer(), name='value')
+        self.dropout_layer = layers.Dropout(self.dropout)
+        
+    def create_attention_mask(self, input_mask):
+        mask = tf.cast(input_mask, tf.float32)
+        return tf.linalg.einsum('aj,ak->ajk', mask, mask)
+        
+    def call(self, inputs, mask=None, training=None):
+        query = self.query_layer(inputs)
+        key   = self.key_layer(inputs)
+        value = self.value_layer(inputs)
+        
+        # Equation 1 of "Attention is all you need"
+        attention_scores = (tf.matmul(query, key, transpose_b=True) 
+                            / tf.sqrt(float(self.units)))
+
+        # zero out masked values
+        attention_mask = self.create_attention_mask(mask)
+        attention_scores = attention_scores + (1. - attention_mask) * -10000.0
+        
+        attention_probs = tf.nn.softmax(attention_scores)
+        attention_probs = self.dropout_layer(attention_probs, training=training)
+        context_layer = tf.matmul(attention_probs, value)
+        
+        return context_layer
+
+    def compute_mask(self, inputs, mask=None):
+        return mask
+
+    def get_config(self):
+        config = super(Attention, self).get_config()
+        config.update({'units': self.units, 'dropout': self.dropout})
+        return config
+
+
+class Transformer(layers.Layer):
+    """ Performs the multi-headed attention and normalization of a single
+    transformer block """
+
+    def __init__(self, num_heads, dropout=0.1, **kwargs):
+        super(Transformer, self).__init__(**kwargs)        
+        self.num_heads = num_heads
+        self.dropout = dropout
+        
+    def build(self, input_shape):
+
+        # Split the model dimension equally amoung attention heads
+        d_model = input_shape[-1]
+        assert d_model % self.num_heads == 0, \
+            f"input dimension {d_model} not divisible by {self.num_heads} "\
+            "attention heads"
+        
+        self.units = d_model // self.num_heads
+        
+        self.attention_heads = [
+            Attention(self.units, self.dropout, name='head_{}'.format(i))
+            for i in range(self.num_heads)]
+
+        self.attention_dense = layers.Dense(d_model,
+                                            kernel_initializer=initializer(),
+                                            activation=gelu)
+        self.projection_dense = layers.Dense(d_model,
+                                             kernel_initializer=initializer(),
+                                             activation=gelu)
+        
+        self.attention_layer_norm = layers.LayerNormalization()
+        self.projection_layer_norm = layers.LayerNormalization()
+        self.dropout_layer = layers.Dropout(self.dropout)
+
+    def call(self, inputs, mask=None, training=None):
+        
+        # Multi-head attention block
+        attention_output = tf.concat([attention_layer(inputs, mask=mask) for
+                                      attention_layer in self.attention_heads],
+                                     axis=-1)
+        attention_output = self.attention_dense(attention_output)
+        attention_output = self.attention_layer_norm(attention_output + inputs)
+        
+        # Projection block
+        projection_output = self.projection_dense(attention_output)
+        projection_output = self.dropout_layer(projection_output,
+                                               training=training)
+        projection_output = self.projection_layer_norm(
+            projection_output + attention_output)
+        
+        return projection_output
+    
+    def compute_mask(self, inputs, mask=None):
+        return mask 
+
+    def get_config(self):
+        config = super(Transformer, self).get_config()
+        config.update({'num_heads': self.num_heads, 'dropout': self.dropout})
+        return config
+
+
+class Bias(layers.Layer):
+    """ Final bias layer added to logits prior to softmax scoring. This layer
+    also clears the _keras_mask attribute from the Transformers in order to
+    allow the custom loss function to work properly. """
+
+    def build(self, input_shape):
+        self.bias = self.add_weight(name='classifier_bias',
+                                    dtype=K.floatx(),
+                                    shape=[input_shape[-1]],
+                                    initializer=tf.zeros_initializer())
+        
+    def call(self, inputs):
+        logits = tf.nn.bias_add(inputs, self.bias)
+        return logits
+        
+    def compute_mask(self, inputs, mask=None):
+        return None
+
+
+def masked_sparse_cross_entropy_loss(y_true, y_pred):
+    """ Computes the mean categorical cross_entropy loss across each batch
+    example, where masked or randomized tokens are specified by nonzero entries
+    in y_true """
+    
+    idx = tf.where(y_true != 0)
+    y_pred_mask = tf.boolean_mask(y_pred, y_true != 0)
+    y_true_mask = tf.boolean_mask(y_true, y_true != 0)
+
+    crossentropy_losses = tf.nn.sparse_softmax_cross_entropy_with_logits(
+        y_true_mask,
+        y_pred_mask)
+
+    return tf.reshape(tf.math.segment_mean(crossentropy_losses, idx[:, 0]),
+                      (-1, 1))
+
+    
+class BERTLearningRateScheduler(Callback):
+    def __init__(self, 
+                 initial_learning_rate=1E-4,
+                 final_learning_rate=0,
+                 num_warmup_steps=int(1E3),
+                 num_training_steps=int(1E6)):
+        """ Implements the linear learning rate warmup and linear learning rate
+        decay used by google in BERT pretraining """
+        
+        self.initial_learning_rate = initial_learning_rate
+        self.final_learning_rate = final_learning_rate
+        self.num_warmup_steps = num_warmup_steps
+        self.num_training_steps = num_training_steps
+        self.global_step = 0
+        
+    def on_train_batch_begin(self, batch, logs=None):
+        
+        self.global_step += 1
+        
+        # Still in warmup
+        if self.global_step <= self.num_warmup_steps:
+            scheduled_lr = self.initial_learning_rate * (
+                self.global_step / self.num_warmup_steps)
+        
+        # Linear decay
+        else:
+            scheduled_lr = self.initial_learning_rate - self.global_step * (
+                (self.initial_learning_rate - self.final_learning_rate)
+                / (self.num_training_steps - self.num_warmup_steps))
+            
+        K.set_value(self.model.optimizer.lr, scheduled_lr)
