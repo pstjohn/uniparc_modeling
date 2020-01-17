813929496a687d63fe342e51b339ffbd465357bf
Peter St. John <pstjohn@br006.pvt.bridges.psc.edu>
2020-01-17 13:23:54 -0500

    Edits to run_model

--- run_model.py
+++ run_model.py
@@ -2,13 +2,21 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser(description='BERT model training')
-parser.add_argument('--modelName', default='albert-xlarge', help='model name for directory saving')
-parser.add_argument('--batchSize', type=int, default=8, help='batch size per gpu')
-parser.add_argument('--stepsPerEpoch', type=int, default=10000, help='steps per epoch')
-parser.add_argument('--warmup', type=int, default=10000, help='warmup steps')
-parser.add_argument('--lr', type=float, default=1E-4, help='initial learning rate')
-parser.add_argument('--weightDecay', type=float, default=0.01, help='AdamW weight decay')
-parser.add_argument('--sequenceLength', type=int, default=1024, help='Protein AA sequence length')
+parser.add_argument('--modelName', default='albert-xlarge', 
+		    help='model name for directory saving')
+parser.add_argument('--batchSize', type=int, default=8, 
+		    help='batch size per gpu')
+parser.add_argument('--stepsPerEpoch', type=int, default=10000, 
+		    help='steps per epoch')
+parser.add_argument('--warmup', type=int, default=10000, 
+		    help='warmup steps')
+parser.add_argument('--lr', type=float, default=1E-4, 
+		    help='initial learning rate')
+parser.add_argument('--weightDecay', type=float, default=0.01, 
+		    help='AdamW weight decay')
+parser.add_argument('--sequenceLength', type=int, default=1024, 
+		    help='Protein AA sequence length')
+
 arguments = parser.parse_args()
 
 import numpy as np
@@ -36,30 +44,8 @@ for gpu in gpus:
 if gpus and is_using_hvd():
     tf.config.experimental.set_visible_devices(gpus[hvd.local_rank()], 'GPU')
 
-from bert.dataset import create_masked_input_dataset
-
 hvd_size = hvd.size() if is_using_hvd() else None
 hvd_rank = hvd.rank() if is_using_hvd() else None
-
-training_data = create_masked_input_dataset(
-    sequence_path='../uniparc_data/sequences_train.txt',
-    max_sequence_length=arguments.sequenceLength,
-    batch_size=arguments.batchSize,
-    fix_sequence_length=False,
-    shard_num_workers=hvd_size,
-    shard_worker_index=hvd_rank)
-
-training_data.repeat().prefetch(tf.data.experimental.AUTOTUNE)
-
-valid_data = create_masked_input_dataset(
-    sequence_path='../uniparc_data/sequences_valid.txt',
-    max_sequence_length=arguments.sequenceLength,
-    batch_size=arguments.batchSize,
-    fix_sequence_length=False,
-    shard_num_workers=hvd_size,
-    shard_worker_index=hvd_rank)
-
-valid_data.repeat().prefetch(tf.data.experimental.AUTOTUNE)
 
 # Create the model
 from bert.model import create_albert_model
@@ -75,9 +61,11 @@ model = create_albert_model(embedding_dimension=128,
 if hvd_rank or not is_using_hvd() == 0:
     model.summary()
     
-from bert.optimizers import ECE, masked_sparse_categorical_crossentropy, BertLinearSchedule
+from bert.optimizers import (ECE, masked_sparse_categorical_crossentropy,
+			     BertLinearSchedule)
     
-opt = tfa.optimizers.AdamW(learning_rate=arguments.lr, weight_decay=arguments.weightDecay)
+opt = tfa.optimizers.AdamW(learning_rate=arguments.lr,
+			   weight_decay=arguments.weightDecay)
 opt = tf.train.experimental.enable_mixed_precision_graph_rewrite(opt)
 
 # Horovod: add Horovod DistributedOptimizer.
@@ -110,9 +98,10 @@ callbacks = [
 
 if is_using_hvd():
     callbacks += [
-    # Horovod: broadcast initial variable states from rank 0 to all other processes.
-    # This is necessary to ensure consistent initialization of all workers when
-    # training is started with random weights or restored from a checkpoint.
+    # Horovod: broadcast initial variable states from rank 0 to all other
+    # processes.  This is necessary to ensure consistent initialization of all
+    # workers when training is started with random weights or restored from a
+    # checkpoint.
     hvd.callbacks.BroadcastGlobalVariablesCallback(0),
 
     # Horovod: average metrics among workers at the end of every epoch.
@@ -121,14 +110,36 @@ if is_using_hvd():
     hvd.callbacks.MetricAverageCallback(),
 ]
 
-# Horovod: save checkpoints only on worker 0 to prevent other workers from corrupting them.
+# Horovod: save checkpoints only on worker 0 to prevent other workers from
+# corrupting them.
 if (not is_using_hvd() or hvd_rank == 0):
     callbacks.append(tf.keras.callbacks.CSVLogger(f'{checkpoint_dir}/log.csv'))
-    callbacks.append(tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix))
+    callbacks.append(tf.keras.callbacks.ModelCheckpoint(
+	    filepath=checkpoint_prefix))
     
 # Horovod: write logs on worker 0.
 verbose = 1 if (hvd_rank == 0 or not is_using_hvd()) else 0
 
-model.fit(training_data, steps_per_epoch=arguments.stepsPerEpoch, epochs=1000,
-          verbose=verbose, validation_data=valid_data, validation_steps=100,
-          callbacks=callbacks)
+from bert.dataset import create_masked_input_dataset
+
+training_data = create_masked_input_dataset(
+    sequence_path='../uniparc_data/sequences_train.txt',
+    max_sequence_length=arguments.sequenceLength,
+    batch_size=arguments.batchSize,
+    shard_num_workers=hvd_size,
+    shard_worker_index=hvd_rank)
+
+training_data = training_data.repeat().prefetch(tf.data.experimental.AUTOTUNE)
+
+valid_data = create_masked_input_dataset(
+    sequence_path='../uniparc_data/sequences_valid.txt',
+    max_sequence_length=arguments.sequenceLength,
+    batch_size=arguments.batchSize,
+    shard_num_workers=hvd_size,
+    shard_worker_index=hvd_rank)
+
+valid_data = valid_data.repeat().prefetch(tf.data.experimental.AUTOTUNE)
+
+model.fit(training_data, steps_per_epoch=arguments.stepsPerEpoch, epochs=10,
+	  verbose=verbose, validation_data=valid_data, validation_steps=100,
+	  callbacks=callbacks)
